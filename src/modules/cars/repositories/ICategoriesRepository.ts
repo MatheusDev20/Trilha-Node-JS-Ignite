8fb1f0e0cb954f73f9b323455573255a1d11f503
Matheus de Paula <matheusdev20@gmail.com>
2021-03-20 15:07:24 -0300

    Spliting in modules and adding Specification Entity

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -6,7 +6,7 @@ interface ICreateCategoryDTO {
 }
 // Classe para ser implementada pelas subclasses, onde não importa como vai ser feita a implementação dos metodos.
 // TypeORM, Sequelize etc...
-interface ICategoriesRepository { 
+interface ICategoriesRepository {
     findByName(name: string): Category;
     list(): Category[];
     create({ name, description }: ICreateCategoryDTO): void;
