a0603a2eb5af2de3e94f3888504f59d4e43b5da0
Matheus <matheusdev20@gmail.com>
2021-03-30 22:08:20 -0300

    Creating Categories Migration and Entities

--- infos.md
+++ infos.md
@@ -5,4 +5,8 @@ docker ps -a -> Lista todos os containers existentes (Up ou não)
 docker rm (id) -> Remover um container (Container precisa estar Down)
 docker start (id/nome) -> Inicia um container
 
+# Comandos Docker Compose
+docker-compose stop -> Para os serviços
+docker-compose start -> Inicia os serviços
+docker-compose down -> Remove a criação do docker-compose
 
--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,16 @@ import swaggerUi from "swagger-ui-express";
 import routes from "./routes/index.routes";
 import swaggerFile from "./swagger.json";
 
+import "./database";
+
 const app = express();
 
 app.use(express.json());
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(routes);
+
 app.listen(3333, () => {
     console.log("Running");
 });
